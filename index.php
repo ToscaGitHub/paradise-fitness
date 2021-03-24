2a2f65e34a72898ad76eec5251a87f4d6810e65d
SCATOLINI Matteo <matteo.scatolini@my-digital-school.org>
2021-03-24 16:41:28 +0100

    second improvement

--- index.php
+++ index.php
@@ -1,25 +1,54 @@
 <?php require_once("includes/header.inc.php"); ?>
 
 
+<div class="landingTop">
 
+    
+    <div class="firstPart">
+        
+        <img src="" alt="">
+        
+        <p>6 Avenue des prés, Annecy</p>
+        
+        <a href="0033658221147" uk-icon="icon: receiver"></a>
+        <p></p>
+        
+        
+        <div class="emailPart">
+            
+            <h1>UN COURS D'ESSAI GRATUIT !</h3>
+            
+            <form action="POST">
+                
+                <div class="uk-inline">
+                    <span class="uk-form-icon" uk-icon="icon: mail"></span>
+                    <input type="email" class="email">
+                </div>
+                
+                
+                <input type="submit" value="S'INSCRIRE">
+                
+            </form>
+            
+        </div>
+        
+        
+    </div>
+    
+    <div class="circle4">
+        <div class="circle3">
+            <div class="circle2">
+                <div class="circle1">
 
+                    <img src="" alt="">
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
 
 
 <?php require_once("includes/footer.inc.php"); ?>
