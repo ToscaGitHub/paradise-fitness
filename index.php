ce1f4a66c523b1a26b465db1c1ba6cfde8c15bf6
SCATOLINI Matteo <matteo.scatolini@my-digital-school.org>
2021-03-25 10:31:26 +0100

    third maj

--- index.php
+++ index.php
@@ -3,51 +3,72 @@
 
 <div class="landingTop">
 
+    <div class="infoTop">
+        
+        <img src="images/logo.svg" alt="logo">
+        
+        <div id="map">
+            <i class="fas fa-map-marked-alt map"></i>
+            <p>6 Avenue des prés, Annecy</p>
+        </div>
+            
+        <div id="phone">
+            <i class="fas fa-phone-alt phone"></i>
+            <p>+33 6 58 22 11 47</p>
+        </div>
+        
+    </div>
     
+
     <div class="firstPart">
-        
-        <img src="" alt="">
-        
-        <p>6 Avenue des prés, Annecy</p>
-        
-        <a href="0033658221147" uk-icon="icon: receiver"></a>
-        <p></p>
-        
-        
-        <div class="emailPart">
             
-            <h1>UN COURS D'ESSAI GRATUIT !</h3>
+        <h1>UN COURS D'ESSAI GRATUIT !</h3>
             
-            <form action="POST">
+        <form action="POST">
                 
-                <div class="uk-inline">
-                    <span class="uk-form-icon" uk-icon="icon: mail"></span>
-                    <input type="email" class="email">
-                </div>
-                
-                
-                <input type="submit" value="S'INSCRIRE">
-                
-            </form>
-            
-        </div>
-        
-        
-    </div>
-    
-    <div class="circle4">
-        <div class="circle3">
-            <div class="circle2">
-                <div class="circle1">
-
-                    <img src="" alt="">
-
-                </div>
+            <div class="uk-inline mail">
+                <span class="uk-form-icon emailIcon" uk-icon="icon: mail; ratio: 2"></span>
+                <input type="email" class="email" id="email">
             </div>
-        </div>
+                
+                
+            <input type="submit" value="S'INSCRIRE !">
+                
+        </form>
+            
     </div>
-    
-    
+        
+
+</div>
+
+<div class="info">
+
+    <h3>NOS AVANTAGES</h3>
+
+    <div class="advantages">
+        
+        <div>
+            <i class="fas fa-calendar-check"></i>
+            <h5>Ouvert de 6h à 00h</h5>
+            <p>du Lundi au Dimanche</p>
+        </div>
+        
+        <div>
+            <i class="fas fa-credit-card"></i>
+            <h5>Sans engagement</h5>
+            <p>résiliation possible à tout moment</p>
+        </div>
+        
+        <div>
+            <img src="images/icons/coach.svg" alt="coach Icon">
+            <h5>Coachs Sportifs</h5>
+            <p>du Lundi au Samedi</p>
+        </div>
+
+
+    </div>
+
+
 </div>
 
 
